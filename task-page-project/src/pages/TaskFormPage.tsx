7cf3b24d626bf0f780fa58054a5b4e295822b904
joankenobi <joan1233blanco@gmail.com>
2024-03-29 20:07:50 -0400

    feat: handle data input for create task

--- task-page-project/src/pages/TaskFormPage.tsx
+++ task-page-project/src/pages/TaskFormPage.tsx
@@ -1,17 +1,36 @@
 /* formulario de imputs para crear una tarea */
+import { useForm } from "react-hook-form"
+
 
 export function TaskFormPage() {
+    const { register, handleSubmit, formState: {errors} } = useForm(); // register y handleSubmit son funciones que se obtienen de useForm, use form debre iterarse para obtener las funciones
+    
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+    })
+    
     return (
         <div>
-            <form>
+            <form 
+            onSubmit={onSubmit} // onSubmit es un evento que se dispara cuando se envia el formulario.
+            >
                 <label>
                     Title:
-                    <input type="text" placeholder="title" />
+                    <input type="text" placeholder="title"
+                        {...register("title", { required: true })}
+                    // Se registra el input con el nombre title y se le pasa un objeto con la propiedad required en true
+                    />
                 </label>
+                {errors.title && <span>Title is required</span>}
                 <label>
                     Description:
-                    <textarea rows="3" placeholder="description" ></textarea>
+                    <textarea rows="3" placeholder="description"
+                        {...register("description", { 
+                            required: true // para tener una mejor validacion se recomienda usar yup
+                        })}
+                        ></textarea>
                 </label>
+                        {errors.description && <span>Title is required</span>}
                 <button type="submit">Save Task</button>
             </form>
         </div>
