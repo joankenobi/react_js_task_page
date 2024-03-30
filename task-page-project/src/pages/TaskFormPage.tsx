b2a286e93ae313073287f29e1c85b7146514a7eb
joankenobi <joan1233blanco@gmail.com>
2024-03-29 20:07:35 -0400

    feat: handle data input for create task

--- task-page-project/src/pages/TaskFormPage.tsx
+++ task-page-project/src/pages/TaskFormPage.tsx
@@ -3,7 +3,17 @@
 export function TaskFormPage() {
     return (
         <div>
-        <h1>Crea la tarea</h1>
+            <form>
+                <label>
+                    Title:
+                    <input type="text" placeholder="title" />
+                </label>
+                <label>
+                    Description:
+                    <textarea rows="3" placeholder="description" ></textarea>
+                </label>
+                <button type="submit">Save Task</button>
+            </form>
         </div>
     );
 }
